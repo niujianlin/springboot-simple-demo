b540a8676ed2b7515ecd4ddfbf66a52dbba79938
niujl <1094776259@qq.com>
2023-01-29 03:11:13 +0800

    发送两条json格式数据给前端，在控制器配置了跨域注释（前8080,后8088），前后端能联通

--- src/main/java/com/example/springboothello/controller/GoodsController.java
+++ src/main/java/com/example/springboothello/controller/GoodsController.java
@@ -2,10 +2,7 @@ package com.example.springboothello.controller;
 
 import com.example.springboothello.entity.Cart;
 import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.spring.web.json.Json;
 
 import java.util.ArrayList;
@@ -14,12 +11,13 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class GoodsController {
     @RequestMapping("/cart")
     public Map<String, Object> getCart(){
         Map<String, Object> cartMap = new HashMap<>(3);
-        Cart cart1 = new Cart(1, "班俏BANQIAO超火ins潮卫衣女士2020秋季新款韩版宽松慵懒风薄款外套带帽上衣", "http://localhost:8080/images/1.png", 108.00, 1, true);
-        Cart cart2 = new Cart(2, "嘉叶希连帽卫衣女春秋薄款2020新款宽松bf韩版字母印花中长款外套ins潮", "http://localhost:8080/images/2.png", 129.00, 1, true);
+        Cart cart1 = new Cart(1, "班俏BANQIAO超火ins潮卫衣女士2020秋季新款韩版宽松慵懒风薄款外套带帽上衣", "http://localhost:8088/images/1.png", 108.00, 1, true);
+        Cart cart2 = new Cart(2, "嘉叶希连帽卫衣女春秋薄款2020新款宽松bf韩版字母印花中长款外套ins潮", "http://localhost:8088/images/2.png", 129.00, 1, true);
         List<Cart> cartList = new ArrayList<>();
         cartList.add(cart1);
         cartList.add(cart2);
